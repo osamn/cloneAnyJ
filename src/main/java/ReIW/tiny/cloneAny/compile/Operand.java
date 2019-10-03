0e949296a063a5703912daa988ea5e52d9b7ae2b
osamn <callee.caller@gmeil.com>
2019-10-03 20:56:51 +0900

    operand のだいたいのしよう

--- src/main/java/ReIW/tiny/cloneAny/compile/Operand.java
+++ src/main/java/ReIW/tiny/cloneAny/compile/Operand.java
@@ -1,23 +1,53 @@
 package ReIW.tiny.cloneAny.compile;
 
+import ReIW.tiny.cloneAny.pojo.Slot;
+
 public interface Operand {
 
+	class New implements Operand {
+		public final String owner;
+
+		New(final String owner) {
+			this.owner = owner;
+		}
+		// NEW
+		// DUP
+	}
+
+	class Init implements Operand {
+		public final String owner;
+		public final String desctiptor;
+
+		Init(final String owner, final String descriptor) {
+			this.owner = owner;
+			this.desctiptor = descriptor;
+		}
+		// INVOKESPECIAL owner <init> desctiptor
+	}
+
 	class LoadLhs implements Operand {
+		// ALOAD 1
 	}
 
 	class LoadRhs implements Operand {
+		// ALOAD 2
 	}
 
 	class StoreRhs implements Operand {
-	}
-
-	class New implements Operand {
-	}
-
-	class InvokeSpecial implements Operand {
+		// ASTORE 2
 	}
 
 	class InvokeGet implements Operand {
+		public final String owner;
+		public final String name;
+		public final String desctiptor;
+
+		InvokeGet(final String owner, final String name, final String desctiptor) {
+			this.owner = owner;
+			this.name = name;
+			this.desctiptor = desctiptor;
+		}
+		// INVOKEVIRTUAL owner name desctiptor
 	}
 
 	class InvokeSet implements Operand {
@@ -29,53 +59,40 @@ public interface Operand {
 	class FieldSet implements Operand {
 	}
 
-	class StartIndexLoop implements Operand {
-		// size をとるための情報が必要
-	}
-
-	class EndIndexLoop implements Operand {
-	}
-
-	class StartKeyLoop implements Operand {
-		// Map#keySet のループになる
-	}
-
-	class EndKeyLoop implements Operand {
-	}
-
-	class CheckMapKeyExists implements Operand {
-		// なかったら例外なので終了側はいらないとおも
-	}
-
-	class TestMapKeyExists implements Operand {
-		// if の開始
-	}
-
-	class MapKeyNotExists implements Operand {
-		// if の終了
-		// たぶんここでラベルつかう感じかな
-	}
-
-	class MapGet implements Operand {
-	}
-
-	class MapPut implements Operand {
-	}
-
-	class ArrayGet implements Operand {
-	}
-
-	class ArraySet implements Operand {
-	}
-
-	class ListGet implements Operand {
-	}
-
-	class ListSet implements Operand {
-	}
-
 	class Convert implements Operand {
+		final Slot lhs;
+		final Slot rhs;
+
+		// box unbox とか cast とか
 		// Ditto とるコードに展開されるよ
+		Convert(Slot lhs, Slot rhs) {
+			this.lhs = lhs;
+			this.rhs = rhs;
+		}
+	}
+
+	class ListToList implements Operand {
+		Convert conv;
+
+	}
+
+	class ListToArray implements Operand {
+		Convert conv;
+	}
+
+	class ArrayToList implements Operand {
+		Convert conv;
+
+	}
+
+	class ArrayToArray implements Operand {
+		Convert conv;
+
+	}
+
+	class MapToMap implements Operand {
+		Convert conv;
+
 	}
 
 }
